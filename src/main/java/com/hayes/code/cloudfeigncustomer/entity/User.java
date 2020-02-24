c7a12f92ad3f2d6b7ac5ef157bc6f0050df660cf
hayes <775788531@qq.com>
2020-02-24 13:11:37 +0800

    commit

--- src/main/java/com/hayes/code/cloudfeigncustomer/entity/User.java
+++ src/main/java/com/hayes/code/cloudfeigncustomer/entity/User.java
@@ -1,20 +1,18 @@
 package com.hayes.code.cloudfeigncustomer.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class User {
 
     private String name ;
     private int age ;
 
-    public User(String name , int age ) {
-        name = name ;
-        age = age ;
-    }
 
 
 }
